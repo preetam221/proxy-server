655c67263a5e268a60c5b78c86b082ff9ec2c3c8
Preetam Paul <82496116+preetam221@users.noreply.github.com>
2023-12-16 22:37:18 +0530

    Update proxy-server-setup.sh

--- proxy-server-setup.sh
+++ proxy-server-setup.sh
@@ -4,17 +4,17 @@
 sudo apt-get update
 
 # Install required packages
-sudo apt-get install -y tinyproxy sed figlet
+sudo apt-get install -y tinyproxy sed figlet wget
 
 # Configure the server
 sudo systemctl enable tinyproxy.service
 sudo cp /etc/tinyproxy/tinyproxy.conf /etc/tinyproxy/tinyproxy.conf.default
-sudo sed -i 's/Allow 127.0.0.1/#Allow 127.0.0.1/g' /etc/squid/squid.conf
-sudo sed -i 's/Allow ::1/#Allow ::1/g' /etc/squid/squid.conf
-sudo sed -i 's/#DisableViaHeader Yes/DisableViaHeader Yes/g' /etc/squid/squid.conf
+sudo sed -i 's/Allow 127.0.0.1/#Allow 127.0.0.1/g' /etc/tinyproxy/tinyproxy.conf
+sudo sed -i 's/Allow ::1/#Allow ::1/g' /etc/tinyproxy/tinyproxy.conf
+sudo sed -i 's/#DisableViaHeader Yes/DisableViaHeader Yes/g' /etc/tinyproxy/tinyproxy.conf
 sudo echo "#
 # MinSpareServers/MaxSpareServers: These settings set the upper and
-# lower limit for the number of spare servers which should be available.
+# lower limit for the number of spare servers that should be available.
 #
 # If the number of spare servers falls below MinSpareServers then new
 # server processes will be spawned.  If the number of servers exceeds
@@ -32,16 +32,23 @@ StartServers 20
 # disables thread reaping. If you do notice problems with memory
 # leakage, then set this to something like 10000.
 #
-MaxRequestsPerChild 1000" >> /etc/squid/squid.conf
+MaxRequestsPerChild 1000" >> /etc/tinyproxy/tinyproxy.conf
 sudo systemctl restart tinyproxy.service
 
 # Edit DNS
 sudo sed -i 's/#DNS=/DNS=1.1.1.1/g' /etc/systemd/resolved.conf
 sudo sed -i 's/#FallbackDNS=/FallbackDNS=1.0.0.1/g' /etc/systemd/resolved.conf
 sudo systemctl restart systemd-resolved
+sudo sed -i 's/nameserver/#nameserver/g' /etc/resolv.conf
+sudo echo "# IPv4 dns
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+# IPv6 dns
+nameserver 2606:4700:4700::1111
+nameserver 2606:4700:4700::1001" >> /etc/resolv.conf
 
 # Success Message
 figlet SUCCESS.
-echo You can now connect to your proxy using the credintials below:
+echo You can now connect to your proxy using the credentials below:
 echo Address: $(wget -qO- icanhazip.com)
 echo Port: 8888
