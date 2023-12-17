5378c0ab880cdc43469fc330437fb3baf9f857a9
Preetam Paul <82496116+preetam221@users.noreply.github.com>
2023-12-17 14:15:41 +0530

    v1.0.1

--- change-proxy-port.sh
+++ change-proxy-port.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-if [ "$UID" == "0" ]
+if [ -e /etc/tinyproxy/tinyproxy.conf.default ]
 then
-    if [ -e /etc/tinyproxy/tinyproxy.conf.default ]
-    then
-        echo Enter your prefered port \(E.g. 443\)
-        read port
-        sudo sed -z 's/#\nPort/#\nPort $port #/g' /etc/tinyproxy/tinyproxy.conf
-    else
-        sudo cp /etc/tinyproxy/tinyproxy.conf /etc/tinyproxy/tinyproxy.conf.default
-        echo Enter your prefered port \(E.g. 443\)
-        read port
-        sudo sed -z 's/#\nPort/#\nPort $port #/g' /etc/tinyproxy/tinyproxy.conf
-    fi
+    echo Enter your prefered port \(E.g. 443\)
+    read port
+    sudo sed -z 's/#\nPort/#\nPort $port #/g' /etc/tinyproxy/tinyproxy.conf
 else
-    echo Run the script as root.
+    sudo cp /etc/tinyproxy/tinyproxy.conf /etc/tinyproxy/tinyproxy.conf.default
+    echo Enter your prefered port \(E.g. 443\)
+    read port
+    sudo sed -z 's/#\nPort/#\nPort $port #/g' /etc/tinyproxy/tinyproxy.conf
 fi
